c6eccec6365c2e6610ea0387f6b40d65760ba757
Shaun S <shaun.sticka@outlook.com>
2021-11-05 20:17:12 -0500

    back on track set up question text display part of main function

--- assets/script.js
+++ assets/script.js
@@ -4,105 +4,114 @@ var quizQuestions = [
     {  
        question: "What serves as a collection of random pieces of related information?",
        answers: {
-           text: "Argument",
-           text: "Method",
-           text: "Array",
-           text: "Function"
+           A: "Argument",
+           B: "Method",
+           C: "Array",
+           D: "Function"
        },
        correctAnswer: "C"
     },
     {
         question: "What symbol denotes an id class selector in CSS?",
         answers: {
-            text: "@",
-            text: "#",
-            text: ".",
-            text: "$"
+            A: "@",
+            B: "#",
+            C: ".",
+            D: "$"
         },
         correctAnswer: "B"
     },
     {
         question: "What command is a best practice before modifying the workflow in the command line?",
         answers: {
-            text: "git branch",
-            text: "git status",
-            text: "git merge",
-            text: "git push"
+            A: "git branch",
+            B: "git status",
+            C: "git merge",
+            D: "git push"
         },
         correctAnswer: "A"
     },
     {
         question: "Which command is executed last in Cascading Style Sheets?",
         answers: {
-            text: "Topmost",
-            text: "Longest",
-            text: "Shortest",
-            text: "Bottommost"
+            A: "Topmost",
+            B: "Longest",
+            C: "Shortest",
+            D: "Bottommost"
         },
         correctAnswer: "D"
     },
     {
         question: "What kind of property is the CSS value 'flex'?",
         answers: {
-            text: "Border",
-            text: "Grid",
-            text: "Display",
-            text: "Font"
+            A: "Border",
+            B: "Grid",
+            C: "Display",
+            D: "Font"
         },
         correctAnswer: "C"
     },
     {
         question: "What allows better optimization for different screen sizes?",
         answers: {
-            text: "Image-rendering",
-            text: "Inline display",
-            text: "JavaScript",
-            text: "Media Queries"
+            A: "Image-rendering",
+            B: "Inline display",
+            C: "JavaScript",
+            D: "Media Queries"
         },
         correctAnswer: "D"
     },
     {
         question: "Which language provides the bones and structure of a website?",
         answers: {
-            text: "CSS",
-            text: "HTML",
-            text: "JavaScript",
-            text: "Java"
+            A: "CSS",
+            B: "HTML",
+            C: "JavaScript",
+            D: "Java"
         },
         correctAnswer: "B"
     },
     {
         question: "How many elements can a class be tied to in HTML?",
         answers: {
-            text: "0",
-            text: "1",
-            text: "2",
-            text: "2+"
+            A: "0",
+            B: "1",
+            C: "2",
+            D: "2+"
         },
         correctAnswer: "D"
     },
     {
         question: "What is the best practice way to type in JavaScript?",
         answers: {
-            text: "All caps",
-            text: "No caps",
-            text: "Camel caps",
-            text: "Cat caps"
+            A: "All caps",
+            B: "No caps",
+            C: "Camel caps",
+            D: "Cat caps"
         },
         correctAnswer: "C"
     }
 ];
 
-// loop through the array to display questions
-for (let i = 0; i < array.length; i++) {
-    // prepare the stem element and display text
+// create function to cycle through questions in array
+var showNext = function() { 
+
+    // make sure new stem appears with content from array
     var questionTextEl = document.createElement("div");
-    questionTextEl.className = "quiz-content";
-    questionTextEl.textContent = quizQuestions.question;
-    quiz-stem.appendChild(questionTextEl);
+    questionTextEl.textContent = quizQuestions[0].question;
+    questionTextEl.className = "quiz-slides";
+    document.getElementById("quiz-stem").appendChild(questionTextEl);
+
+    // create container div to display four option buttons
+    var btnContainerEl = document.createElement("div");
+    btnContainerEl.className = "quiz-options";
+
+    // create each button option
+    var btnOption1 = document.createElement("button");
+    edit
+
 
 }
-
 
 
 
